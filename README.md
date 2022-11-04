3bb877fb1651bcf9c39e362d884aedae4e2e65fd
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-04 17:21:32 -0400

    Add inputs together and send result back to dom

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Express
 <p>
+<h2>Running javascript on server side</h2>
 Dependencies:
 express --save <br />
 (global)nodemon (auto refreshes server)<br />
--- calculator.js
+++ calculator.js
@@ -1,17 +1,23 @@
-const express = require('express')
+const express = require('express') //express package
 const app = express()
 const bodyParser = require('body-parser') //body-parser package
 const port = 3000
 
+//get access to form data
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  //console.log(__dirname) - get path to file no matter if localhost or on server
   res.sendFile(__dirname + '/index.html');
 })
 
 app.post('/', (req, res) => {
-  res.send('Thanks for posting!');
+  //use Number as currently adds them as a string
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  let result = num1 + num2;
+
+  res.send('Thanks for posting! <br /> The result of your calculator is ' + result);
 })
 
 app.listen(port, () => {
