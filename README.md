edb28a904fc7b4934ff4a58b19a47ab9f71eade3
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-11-04 16:46:12 -0400

    added multiple routes, added nodemon globally

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Express" 
+# Express
+<p>
+Dependencies: npm install express --save
+(global)npm install -g nodemon (auto refreshes server) 
+</p>
--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//Display information at root of :3000
+//Display information at root(3000)
 app.get('/', (req, res) => {
-  console.log(req); //View request on cline
-  console.log(res);  // view response on cline
-  res.send('<h1>Example Response</h1>');
+  res.send('<h1>Home page</h1>');
 })
 
+//localhost:300/contacts
+app.get('/contacts', (req, res) => {
+  res.send('<h1>Contact Chiefs Management</h1>');
+})
+
+//localhost:300/about
+app.get('/about', (req, res) => {
+  res.send('<h1>About the Chiefs</h1><br /><h2>Kansas City Chiefs</h2>');
+})
+
+//installed nodemon so it auto refreshes server anytime it changes
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
